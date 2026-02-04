e5c4d432d156439140a9d21130bdcc859c093098
Mendez <yahirelian.mendez@outlook.com>
2026-02-04 08:50:22 +0100

    corrigiendo errores en imagenes

--- game_players/models/game_rank.py
+++ game_players/models/game_rank.py
@@ -2,10 +2,11 @@ from odoo import models, fields
 
 class GameRank(models.Model):
     _name = 'game.rank'
+    _inherit = ['image.mixin']
     _description = 'Rango de Jugador'
     _order = 'min_points desc'
 
     name = fields.Char(string='Nombre del Rango', required=True)
     min_points = fields.Integer(string='Puntos Mínimos', required=True)
-    image = fields.Image(string='Insignia', max_width=128, max_height=128)
+    image = fields.Image("Insignia", related="image_1920", store=True)
     description = fields.Text(string='Descripción')
